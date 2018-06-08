d5f0738945b86b4bcdfe936595707562d175ebc7
hs <huangshuai@utianqi.com>
2018-06-08 16:14:04 +0800

    yyy

---
+++ test.js
@@ -0,0 +1,14 @@
+const Web3   = require('web3')
+const web3   = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const eth    = web3.eth;
+
+const account = eth.accounts[0]
+
+
+
+console.log(`账户：${account}`)
+console.log(`区块数：${eth.blockNumber}`)
+eth.getBalance(account, function(err,result) {
+    console.log(`余额：${err||result.toNumber()}`);
+});
+
